3a2a8ceb368cfc7ccd85bdf2a1d275356f088255
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-05-04 22:58:39 +0700

    feat: implement brain-even

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,29 @@
 import readLineSync from 'readline-sync';
 
-export default function hello() {
-  const name = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+export function hello() {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readLineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  return playerName;
+}
+
+export function win(playerName) {
+  console.log(`Congratulations, ${playerName}!`);
+}
+
+export function lose(playerName) {
+  console.log(`Let's try again, ${playerName}!`);
+}
+
+export default function playGame(rounds, game) {
+  const playerName = hello();
+
+  for (let i = 0; i < rounds; i += 1) {
+    if (game()) {
+      console.log('Correct!');
+    } else {
+      return lose(playerName);
+    }
+  }
+  return win(playerName);
 }
---
+++ src/even.js
@@ -0,0 +1,27 @@
+import readLineSync from 'readline-sync';
+import playGame from './cli.js';
+
+function getRandomFromHunded() {
+  const number = Math.floor(Math.random(0, 100) * 100);
+  return number;
+}
+
+function isEven(number) {
+  const result = number % 2 === 0 ? 'yes' : 'no';
+  return result;
+}
+
+function playEvenOnce() {
+  const numberToCheck = getRandomFromHunded();
+  const rightAnswer = isEven(numberToCheck);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log(`Question: ${numberToCheck}`);
+  const playerAnswer = readLineSync.question('Your answer: ');
+
+  return playerAnswer === rightAnswer;
+}
+
+export default function checkEven() {
+  playGame(3, playEvenOnce);
+}
