33d83b0003afb7dba4415ac1c13f4a0cd219eb19
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-05-06 23:07:03 +0700

    fix: remove hello duplicate

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,3 @@
-import readLineSync from 'readline-sync';
+import { hello } from './index.js';
 
-export default function hello() {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  return playerName;
-}
+hello();
